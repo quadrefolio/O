21e64c698c692cba3d9b6fd753554356a92fd78e
quadrefolio <abdelrahmanelsayed036@gmail.com>
2024-05-17 21:30:40 +0300

    the first commit

--- main.c
+++ main.c
@@ -2,7 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+typedef struct 
+{
+    int min;
+    int max;
+    
+}MemoryBoundaries;
+typedef struct 
+{
+    int Pid;
+    char Pstate [32];
+    int Priority;
+    int PC;
+    MemoryBoundaries Memoryboundaries;
+}PCB;
+
+
+
+
+
+void translate(){
+    
+}
+
+
+
 int main(){
-    printf("hi");
+    PCB b ;
+    b.Memoryboundaries.max=10;
+    char hi [32]="hello";
+    printf("hi %d",b.Memoryboundaries.max);
     return 0;
 }
