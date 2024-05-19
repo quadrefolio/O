f9205b7a9e3f938f2aff41917fc6ef98f39ade5f
seifo195 <seifeldin.abdullah@student.guc.edu.eg>
2024-05-19 22:48:11 +0300

    all done

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #include <stdlib.h>
 #define MEMORY_SIZE 61
+#define NUM_INST 3
 
 typedef struct 
 {
     int min;
     int max;
-    
-}MemoryBoundaries;
-
+} MemoryBoundaries;
 
 typedef struct 
 {
@@ -18,13 +17,13 @@ typedef struct
     char state [32];
     int PC;
     MemoryBoundaries Memoryboundaries;
-}PCB;
+} PCB;
 
 typedef struct 
 {
     char name [32];
     char data [64];
-}Memory;
+} Memory;
 
 
 int quanta;
@@ -33,6 +32,9 @@ int arrival_2;
 int arrival_3;
 int clock;
 Memory *memoryArray;
+PCB programs[NUM_INST];
+
+int memIndex = 0;
 
 void initializeMemoryArray() {
     memoryArray = (Memory *)malloc(MEMORY_SIZE * sizeof(Memory));
@@ -49,6 +51,15 @@ void initializeMemoryArray() {
 
 }
 
+void printMemoryArray() {
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        printf("Memory %d\n", i);
+        printf("Name: %s\n", memoryArray[i].name);
+        printf("Data: %s\n", memoryArray[i].data);
+        printf("\n");
+    }
+}
+
 void addProcess(int processID, int arrivalTime, const char *filename) {
     PCB newProcess;
     newProcess.pID = processID;
@@ -61,29 +72,46 @@ void addProcess(int processID, int arrivalTime, const char *filename) {
         return;
     }
 
+    snprintf(memoryArray[memIndex].name, 64 , "Process ID");
+    snprintf(memoryArray[memIndex].data, 64, "%d", processID);
+
+    memIndex++;
+
+    snprintf(memoryArray[memIndex].name, 64 , "Process state");
+    snprintf(memoryArray[memIndex].data, 64, "READY");
+
+    memIndex++;
+
+    snprintf(memoryArray[memIndex].name, 64 , "PC");
+    snprintf(memoryArray[memIndex].data, 64, "%d", newProcess.PC);
+
+    memIndex++;
+
+    snprintf(memoryArray[memIndex].name, 64 , "Boundaries");
+    snprintf(memoryArray[memIndex].data, 64, "%d", newProcess.Memoryboundaries.min);
+
+    memIndex++;
+
 
     char line[64];
-    int lineCount = 0;
+
+    //Number of instructions in the program
+    int lineCount = 4;
+
     while (fgets(line, sizeof(line), file)) {
         snprintf(memoryArray[lineCount].name, 64 , "Instruction");
         snprintf(memoryArray[lineCount].data, 64, line);
-        printf("%s: ",memoryArray[lineCount].name);
-        printf("%s \n",memoryArray[lineCount].data);
         lineCount++;
-
     }
+
+    memIndex = newProcess.Memoryboundaries.max + 1;
+    programs[processID] = newProcess;
+
+    printMemoryArray();
 
     fclose(file);
 
 }
-
-
-
-
-
-
-
-
 
 int main(){
     initializeMemoryArray();
