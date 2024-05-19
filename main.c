5e16973ef2191d49086d3e70d61d2b640e3e8749
seifo195 <seifeldin.abdullah@student.guc.edu.eg>
2024-05-19 20:23:02 +0300

    daw

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
-
-
-
-MEMORY_SIZE = 61;
-Memory memoryArray[MEMORY_SIZE];
-
+#define MEMORY_SIZE 61
 
 typedef struct 
 {
@@ -21,13 +14,11 @@ typedef struct
 
 typedef struct 
 {
-    int Pid;
-    char Pstate [32];
-    int Priority;
+    int pID;
+    char state [32];
     int PC;
     MemoryBoundaries Memoryboundaries;
 }PCB;
-
 
 typedef struct 
 {
@@ -36,29 +27,66 @@ typedef struct
 }Memory;
 
 
+int quanta;
+int arrival_1;
+int arrival_2;
+int arrival_3;
+int clock;
+Memory *memoryArray;
+
 void initializeMemoryArray() {
-
-    for (int i = 0; i < MEMORY_SIZE; i++) {
-        strcpy(memoryArray[i].name, " ");
-        strcpy(memoryArray[i].data, " ");
+    memoryArray = (Memory *)malloc(MEMORY_SIZE * sizeof(Memory));
+    if (memoryArray == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
     }
+    
+    // Initialize the structs in the array
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        snprintf(memoryArray[i].name, 64 , "");
+        snprintf(memoryArray[i].data, 64, "");
+    }
+
+}
+
+void addProcess(int processID, int arrivalTime, const char *filename) {
+    PCB newProcess;
+    newProcess.pID = processID;
+    newProcess.PC = 0;
+    strcpy(newProcess.state, "Ready");
+
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
+
+
+    char line[64];
+    int lineCount = 0;
+    while (fgets(line, sizeof(line), file)) {
+        snprintf(memoryArray[lineCount].name, 64 , "Instruction");
+        snprintf(memoryArray[lineCount].data, 64, line);
+        printf("%s: ",memoryArray[lineCount].name);
+        printf("%s \n",memoryArray[lineCount].data);
+        lineCount++;
+
+    }
+
+    fclose(file);
+
 }
 
 
 
 
 
-
-void translate(){
-    
-    
-}
 
 
 
 
 int main(){
     initializeMemoryArray();
-    
+    addProcess(1, 0,"Program_1.txt");
     return 0;
 }
